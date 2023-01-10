76da922e1e1da17776d2dbf54da6063b18355b3b
Nihelv <121417846+nihelv@users.noreply.github.com>
2023-01-10 16:42:11 +0900

    [D1] Title: 아주 간단한 계산기, Time: 144 ms, Memory: 58,528 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.py
@@ -0,0 +1,14 @@
+a, b = list(map(int,input().split()))
+
+if 0 < a and b <10:
+    z=a+b
+    print(z)
+    z=0
+    z=a-b
+    print(z)
+    z=0
+    z=a*b
+    print(z)
+    z=0
+    z=a/b
+    print(int(z))
