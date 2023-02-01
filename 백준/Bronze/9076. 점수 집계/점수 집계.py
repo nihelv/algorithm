d9ae144df01c2d240eb8f31952725842fbbf4908
Nihelv <121417846+nihelv@users.noreply.github.com>
2023-02-01 15:54:12 +0900

    [Bronze II] Title: 점수 집계, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9076. 점수 집계/점수 집계.py
@@ -0,0 +1,11 @@
+t=int(input())
+
+for i in range(t):
+    num=list(map(int, input().split()))
+    nums=sorted(num)
+    if nums[-2] - nums[1] >= 4:
+        print('KIN')
+    else:
+        nums.pop(-1)
+        nums.pop(0)
+        print(sum(nums))
