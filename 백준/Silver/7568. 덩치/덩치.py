336f6770c11eef35fa5cdbdc267ed67eca9c377d
Nihelv <121417846+nihelv@users.noreply.github.com>
2023-02-07 16:33:18 +0900

    [Silver V] Title: 덩치, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,13 @@
+n = int(input())
+l = []
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    l.append((x, y))
+
+for i in l:
+    rank = 1
+    for j in l:
+        if i[0] < j[0] and i[1] < j[1]:
+                rank += 1
+    print(rank, end = " ")
