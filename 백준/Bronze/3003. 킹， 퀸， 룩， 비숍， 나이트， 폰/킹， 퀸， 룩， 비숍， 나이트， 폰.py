de9456143bae65a1a66ab0c8c3f49d5bfb4700d2
Nihelv <121417846+nihelv@users.noreply.github.com>
2023-02-18 23:46:04 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,15 +1,6 @@
-n=list(map(int, input().split()))
+pieces = [1, 1, 2, 2, 2, 8]
+input_pieces = list(map(int, input().split()))
 
+result = [str(pieces[i] - input_pieces[i]) for i in range(6)]
 
-c=[1, 1, 2, 2, 2, 8]
-c1=[]
-if n != c:
-    for _ in range(len(n)):
-        if n[_] < c[_]:
-            c1.append(c[_] - n[_])
-        elif n[_] > c[_]:
-            c1.append(c[_] - n[_])
-        else:
-            c1.append(0)
-
-print(*c1)
+print(' '.join(result))
