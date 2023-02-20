adff04365843d25acd0d57116d2bdab39e343ceb
Nihelv <121417846+nihelv@users.noreply.github.com>
2023-02-20 16:49:22 +0900

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -8,8 +8,7 @@ a1.APNT_NO
 from APPOINTMENT a1
 left outer join DOCTOR a2 on a1.MDDR_ID = a2.DR_ID
 left outer join PATIENT a3 on a1.PT_NO = a3.PT_NO
-where 1=1
-and a1.APNT_CNCL_YN = 'N'
+where a1.APNT_CNCL_YN = 'N'
 and date(a1.APNT_YMD) = '2022-04-13'
 and a2.MCDP_CD = 'CS'
 order by a1.APNT_YMD
