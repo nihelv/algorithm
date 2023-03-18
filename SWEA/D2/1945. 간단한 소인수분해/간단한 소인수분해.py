506b475f3e71c3c5c6236ef7933e185e96562b67
Nihelv <121417846+nihelv@users.noreply.github.com>
2023-03-18 18:59:29 +0900

    [D2] Title: 간단한 소인수분해, Time: 141 ms, Memory: 58,260 KB -BaekjoonHub

---
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -0,0 +1,23 @@
+T = int(input())
+
+for t in range(1, T+1):
+    N = int(input())
+
+    a, b, c, d, e = 0, 0, 0, 0, 0
+    while N % 2 == 0:
+        a += 1
+        N //= 2
+    while N % 3 == 0:
+        b += 1
+        N //= 3
+    while N % 5 == 0:
+        c += 1
+        N //= 5
+    while N % 7 == 0:
+        d += 1
+        N //= 7
+    while N % 11 == 0:
+        e += 1
+        N //= 11
+
+    print(f"#{t} {a} {b} {c} {d} {e}")
